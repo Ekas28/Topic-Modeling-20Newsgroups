2b9e91bc174ab201e653610c1ab238fac3886962
Ekas28 <125024903+Ekas28@users.noreply.github.com>
2025-07-23 23:58:35 +0530

    Create preprocessing.py

---
+++ src/preprocessing.py
@@ -0,0 +1,12 @@
+import re
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+stop_words = set(stopwords.words('english'))
+lemmatizer = WordNetLemmatizer()
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r'[^a-z\s]', '', text)
+    words = [lemmatizer.lemmatize(word) for word in text.split() if word not in stop_words]
+    return ' '.join(words)
